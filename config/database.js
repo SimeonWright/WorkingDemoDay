91b7d1dd6dbaad009dd13a46cd2a7ac875f9b122
Simeon Wright <simeonwright@Simeons-Air.lan>
2022-11-12 00:35:47 -0500

    working demo day

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://Simeon21:DeaconWright21@cluster0.asleqro.mongodb.net/DemoDay?retryWrites=true&w=majority', 
-    'dbName': 'DemoDay'
+    'url' : 'mongodb+srv://Simeon21:DeaconWright21@cluster0.xcrbf88.mongodb.net/newdemoday?retryWrites=true&w=majority', 
+    'dbName': 'newdemoday'
 };
